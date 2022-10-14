768026bc36da83ce328a58bfad726764fa3e3ff4
Riya-Bisht <manasi.riya2003@gmail.com>
2022-10-14 17:51:49 +0530

    removed useless text

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+      )    );
   }
 }
 
