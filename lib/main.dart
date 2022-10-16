13711d275e14238176947c8ff4735b962acafe7a
Snehit Sah <snehitsah@protonmail.com>
2022-10-16 17:11:35 +0530

    Fix error
    Signed-off-by: Snehit Sah <snehitsah@protonmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,12 @@ import 'package:flutter/material.dart';
 import 'package:tgfs_ui/vars.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+      title: 'Tag File Management',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -36,11 +41,13 @@ class _MyAppState extends State<MyApp> {
     filePaths = ["/home/lain", "/mnt/hd1/music", "/home/chococandy/Pictures"];
 
     editTagTextController = TextEditingController();
+    createTagTextController = TextEditingController();
     super.initState();
   }
 
   void dispose() {
     editTagTextController.dispose();
+    createTagTextController.dispose();
 
     super.dispose();
   }
@@ -48,180 +55,174 @@ class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Tag File Management',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-          body: Row(
-            children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: Container(
-                  padding: const EdgeInsets.all(30),
-                  height: 1000,
-                  width: 400,
-                  color: mainColor,
-                  child: Column(
+    return Scaffold(
+      body: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: const EdgeInsets.all(30),
+              height: 1000,
+              width: 400,
+              color: mainColor,
+              child: Column(
 //column properities:
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    //column children:
-                    children: [
-                      //first column
-                      Expanded(
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: <Widget>[
-                            const Text(
-                              'TAGS',
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 40,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            Expanded(child: Container()),
-                            IconButton(
-                              onPressed: () {
-                                fetchTags();
-                              },
-                              icon: const Icon(Icons.refresh),
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                //column children:
+                children: [
+                  //first column
+                  Expanded(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        const Text(
+                          'TAGS',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
                               color: Colors.white,
-                            ),
-                            IconButton(
-                              onPressed: () {
-                                // Implement add tag
-                                //addTags(tagName);
-                                print("Add button pressed");
-                                addTagDialog(
-                                            context);
-                              },
-                              icon: const Icon(Icons.add),
-                              color: Colors.white,
-                            )
-                          ],
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold),
                         ),
-                      ),
+                        Expanded(child: Container()),
+                        IconButton(
+                          onPressed: () {
+                            fetchTags();
+                          },
+                          icon: const Icon(Icons.refresh),
+                          color: Colors.white,
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            // Implement add tag
+                            //addTags(tagName);
+                            print("Add button pressed");
+                            addTagDialog(context);
+                          },
+                          icon: const Icon(Icons.add),
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                  ),
 
 //2nd Column:
 
-                      Expanded(
-                        child: ListView.builder(
-                            itemCount: tagList.length,
-                            itemBuilder: (context, i) {
-                              return Container(
-                                padding: const EdgeInsets.all(10),
-                                child: Row(
-                                  children: [
-                                    Expanded(
-                                      child: TextButton(
-                                          onPressed: () {
-                                            // Implement tag click
-                                            print(
-                                                "tag ${tagList.elementAt(i)} clicked");
-                                          },
-                                          child: Text(
-                                            tagList.elementAt(i),
-                                            textAlign: TextAlign.left,
-                                            style: const TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 40,
-                                                fontStyle: FontStyle.italic),
-                                          )),
-                                    ),
-                                    IconButton(
-                                      onPressed: () {
-                                        deleteTag(tagList.elementAt(i));
-                                      },
-                                      icon: const Icon(Icons.remove),
-                                      color: Colors.white,
-                                    ),
-                                    const SizedBox(width: 12),
-                                    IconButton(
-                                      onPressed: () {
-                                        editTagDialog(
-                                            context, tagList.elementAt(i));
-                                      },
-                                      icon: const Icon(Icons.edit),
-                                      color: Colors.white,
-                                    ),
-                                  ],
-                                ),
-                              );
-                            }),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                  flex: 2,
-                  child: Container(
-                    padding: const EdgeInsets.all(30),
-                    height: 1000,
-                    width: 800,
-                    color: shadeColor,
-                    child: Column(children: [
-                      Expanded(
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: tagList.length,
+                        itemBuilder: (context, i) {
+                          return Container(
+                            padding: const EdgeInsets.all(10),
+                            child: Row(
                               children: [
-                            const Text('FOLDERS',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    fontSize: 40, fontWeight: FontWeight.bold)),
-                            const SizedBox(height: 30),
-                            ElevatedButton(
-                              style: style,
-                              onPressed: () {},
-                              child: const Text('Add Files'),
+                                Expanded(
+                                  child: TextButton(
+                                      onPressed: () {
+                                        // Implement tag click
+                                        print(
+                                            "tag ${tagList.elementAt(i)} clicked");
+                                      },
+                                      child: Text(
+                                        tagList.elementAt(i),
+                                        textAlign: TextAlign.left,
+                                        style: const TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 40,
+                                            fontStyle: FontStyle.italic),
+                                      )),
+                                ),
+                                IconButton(
+                                  onPressed: () {
+                                    deleteTag(tagList.elementAt(i));
+                                  },
+                                  icon: const Icon(Icons.remove),
+                                  color: Colors.white,
+                                ),
+                                const SizedBox(width: 12),
+                                IconButton(
+                                  onPressed: () {
+                                    editTagDialog(
+                                        context, tagList.elementAt(i));
+                                  },
+                                  icon: const Icon(Icons.edit),
+                                  color: Colors.white,
+                                ),
+                              ],
                             ),
-                          ])),
-                      Expanded(
-                        child: ListView.builder(
-                          //fileNames and filePaths
-                          itemCount: fileNames.length,
-                          itemBuilder: (context, i) {
-                            return Container(
-                              padding: const EdgeInsets.all(10),
-                              child: Column(
-                                children: [
-                                  Text(
-                                    fileNames.elementAt(i),
-                                    textAlign: TextAlign.left,
-                                    style: const TextStyle(
-                                        color: Color.fromARGB(255, 18, 15, 15),
-                                        fontSize: 18,
-                                        fontStyle: FontStyle.italic),
-                                  ),
-                                  Text(
-                                    filePaths.elementAt(i),
-                                    textAlign: TextAlign.left,
-                                    style: const TextStyle(
-                                        color: Color.fromARGB(255, 18, 15, 15),
-                                        fontSize: 18,
-                                        fontStyle: FontStyle.italic),
-                                  )
-                                ],
-                              ),
-                            );
-                          },
-                        ),
-                      )
-                    ]),
-                  ))
-            ],
+                          );
+                        }),
+                  )
+                ],
+              ),
+            ),
           ),
-          // margin: const EdgeInsets.all(10),
-          // padding: const EdgeInsets.all(10),
-          // color: Color.fromARGB(255, 144, 178, 205),
-          // height: 1000,
-          // width: 700,
-        ));
+          Expanded(
+              flex: 2,
+              child: Container(
+                padding: const EdgeInsets.all(30),
+                height: 1000,
+                width: 800,
+                color: shadeColor,
+                child: Column(children: [
+                  Expanded(
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                        const Text('FOLDERS',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.bold)),
+                        const SizedBox(height: 30),
+                        ElevatedButton(
+                          style: style,
+                          onPressed: () {},
+                          child: const Text('Add Files'),
+                        ),
+                      ])),
+                  Expanded(
+                    child: ListView.builder(
+                      //fileNames and filePaths
+                      itemCount: fileNames.length,
+                      itemBuilder: (context, i) {
+                        return Container(
+                          padding: const EdgeInsets.all(10),
+                          child: Column(
+                            children: [
+                              Text(
+                                fileNames.elementAt(i),
+                                textAlign: TextAlign.left,
+                                style: const TextStyle(
+                                    color: Color.fromARGB(255, 18, 15, 15),
+                                    fontSize: 18,
+                                    fontStyle: FontStyle.italic),
+                              ),
+                              Text(
+                                filePaths.elementAt(i),
+                                textAlign: TextAlign.left,
+                                style: const TextStyle(
+                                    color: Color.fromARGB(255, 18, 15, 15),
+                                    fontSize: 18,
+                                    fontStyle: FontStyle.italic),
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                ]),
+              ))
+        ],
+      ),
+      // margin: const EdgeInsets.all(10),
+      // padding: const EdgeInsets.all(10),
+      // color: Color.fromARGB(255, 144, 178, 205),
+      // height: 1000,
+      // width: 700,
+    );
   }
 
   void fetchTags() async {
@@ -268,14 +269,15 @@ class _MyAppState extends State<MyApp> {
     fetchTags();
   }
 
- Future addTagDialog(BuildContext context) => showDialog(
+  Future addTagDialog(BuildContext context) => showDialog(
       context: context,
-      builder: (context) => AlertDialog(
+      builder: (BuildContext context) => AlertDialog(
             title: Text("Add TAG"),
             content: TextField(
               autofocus: true,
               controller: createTagTextController,
-              decoration: const InputDecoration(hintText: 'Enter new tag name:'),
+              decoration:
+                  const InputDecoration(hintText: 'Enter new tag name:'),
             ),
             actions: [
               TextButton(
@@ -295,5 +297,4 @@ class _MyAppState extends State<MyApp> {
               )
             ],
           ));
-
 }
